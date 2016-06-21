71d648d5d0b54833f9454e2a5bbe23326b6f2c94
Jesse Hattabaugh <arkanciscan@gmail.com>
2016-06-21 23:48:09 +0000

    minifyifying

--- index.js
+++ index.js
@@ -11,49 +11,55 @@ const watchify = require('watchify');
 const errorify = require('errorify');
 const eslintify = require('eslintify');
 const babelify = require('babelify');
+const minifyify = require('minifyify');
 
 const SRC = 'src';
 const DEST = 'public';
 const ARGS = parseArgs(process.argv.slice(2));
 
-const B = browserify(
-  path.join(SRC, 'main.js'),
-  ARGS.dev ? {
-    cache: {},
-    packageCache: {},
-    plugin: [
-      watchify,
-      errorify
-    ]
-  } : {}
-)
-  .transform(babelify, {presets: ['es2015']})
+const config = {
+  debug: true
+};
+
+if (ARGS.watch) {
+  config.cache = {};
+  config.packageCache = {};
+  config.plugin = [watchify, errorify];
+}
+
+const bundler = browserify(path.join(SRC, 'main.js'), config)
+  .plugin(minifyify, {
+    map: 'index.js.map',
+    output: path.join(DEST, 'index.js.map')
+  })
+  .transform(babelify, {
+    presets: ['es2015']
+  })
   .transform(eslintify);
 
 function bundle() {
-  B.bundle()
+  bundler.bundle()
     .pipe(fs.createWriteStream(path.join(DEST, 'index.js')));
 }
 
 /** clear old build products */
-rm(path.join(__dirname, DEST), function () {
-  mkdir(path.join(__dirname, DEST), function () {
-    
+rm(path.join(__dirname, DEST), function() {
+  mkdir(path.join(__dirname, DEST), function() {
+
     /** compile the index.html file */
     fs.writeFile(
-      path.join(DEST, 'index.html'), 
+      path.join(DEST, 'index.html'),
       jade.renderFile(path.join(SRC, 'main.jade'), {
         cache: false,
-        pretty: ARGS.dev
+        pretty: false
       })
-    ); 
-    
+    );
+
     /** bundle the index.js file */
     bundle();
-    if(ARGS.dev) B.on('update', bundle);
-      
+
+    /** rebundle if source changes */
+    if (ARGS.watch) bundler.on('update', bundle);
+
   })
 });
-
-
-
